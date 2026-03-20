7f96087028b9762dd3906085a4bad3e2941521d8
lotanna igwilo <cholovitolo@yahoo.com>
2026-03-20 10:33:20 -0400

    Final commit message:

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
- 
-router.get("/about", protect, (req, res) => res.render("pages/about"));
-router.get("/contact", protect, (req, res) => res.render("pages/contact"));
- 
+
+router.get("/about", protect, (req, res) => res.render("about"));
+router.get("/contact", protect, (req, res) => res.render("contact"));
+
 export default router;
